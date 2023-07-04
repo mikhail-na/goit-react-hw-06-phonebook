5cce5e33e7f9f8376f071f52a418fdaf33a83862
mikhail-na <115450336+mikhail-na@users.noreply.github.com>
2023-07-04 15:17:16 +0200

    corrected form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,29 @@ import { Layout } from './Layout/Layout';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactFilter } from './ContactFilter/ContactFilter';
 import { ContactsList } from './ContactsList/ContactsList';
+import { useDispatch, useSelector } from 'react-redux';
+import { getContactsState } from 'redux/selectors';
+import { addContact } from 'redux/contactsSlice';
 
 export const App = () => {
+  const dispatch = useDispatch();
+	const contacts = useSelector(getContactsState);
+	
+
+  const addContactToContacts = data => {
+    	const currentContact = contacts.find(({name}) =>
+			name.toLowerCase().includes(data.name.toLowerCase().trim())
+		);
+    currentContact
+      ? alert(`You already have ${(data.name).toUpperCase()} in your contacts! Please try to change the name of the contact!`)
+      : dispatch(addContact(data))
+		 
+	};
 
   return (
     <>
       <Layout title="Phonebook">
-        <ContactForm />
+        <ContactForm onSubmit={addContactToContacts}/>
       </Layout>
       <Layout title="Contacts">
         <ContactFilter />
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,35 +1,67 @@
 import React from 'react';
+import { useState } from 'react';
 import { nanoid } from '@reduxjs/toolkit';
 import { Form, Input, Label, Button } from './ContactForm.module';
-import { useDispatch, useSelector } from 'react-redux';
-import { addContact } from 'redux/contactsSlice';
-import { getContactsState } from 'redux/selectors';
+
+// import { useDispatch, useSelector } from 'react-redux';
+// import { addContact } from 'redux/contactsSlice';
+// import { getContactsState } from 'redux/selectors';
 
 
-export const ContactForm = () => {
-  const dispatch = useDispatch();
-  const contacts = useSelector(getContactsState);
+export const ContactForm = ({onSubmit}) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
+  // const dispatch = useDispatch();
+  // const contacts = useSelector(getContactsState);
+
+  const reset = () => {
+    setName('');
+    setNumber('');
+  };
+
+  const newContact = (name, number) => {
+    return {
+      id: nanoid(),
+      name,
+      number,
+    }
+  };
+
+  const onInputChange = e => {
+		const { value } = e.currentTarget;
+		e.currentTarget.name === 'name' ? setName(value) : setNumber(value);
+  };
+
+  
   const onFormSubmit = e => {
     e.preventDefault();
-     
-    const form = e.target;
-    const currentName = e.target.elements.name.value;
 
-    const newContact = {
-      id: nanoid(),
-      name: e.target.elements.name.value,
-      number: e.target.elements.number.value,
-    };
-
-    if (contacts.some(({ name }) => name === currentName)) {
-      return alert(`You already have ${(currentName).toUpperCase()} in your contacts! Please try to change the name of the contact!`);
-    }
-    
-    dispatch(addContact(newContact));
-
-    form.reset();
+    const contact = newContact(name, number);
+    onSubmit(contact);
+    reset();
   };
+
+  // const onFormSubmit = e => {
+  //   e.preventDefault();
+     
+  //   const form = e.target;
+  //   const currentName = e.target.elements.name.value;
+
+  //   const newContact = {
+  //     id: nanoid(),
+  //     name: e.target.elements.name.value,
+  //     number: e.target.elements.number.value,
+  //   };
+
+  //   if (contacts.some(({ name }) => name === currentName)) {
+  //     return alert(`You already have ${(currentName).toUpperCase()} in your contacts! Please try to change the name of the contact!`);
+  //   }
+    
+  //   dispatch(addContact(newContact));
+
+  //   form.reset();
+  // };
 
 
   return (
@@ -42,7 +74,8 @@ export const ContactForm = () => {
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
-          value={contacts.name}
+          value={name}
+          onChange={onInputChange}
           placeholder='Enter Name...'      
         />
       </Label>
@@ -54,7 +87,8 @@ export const ContactForm = () => {
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
-          value={contacts.number}
+          value={number}
+          onChange={onInputChange}
           placeholder="Enter number..."
         />
       </Label>
